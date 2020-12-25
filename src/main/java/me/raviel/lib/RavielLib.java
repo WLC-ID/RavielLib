c166deb7f8b836a2f916e6ed974caccfe1092df5
ravielze <ravielz20@gmail.com>
2020-12-25 16:26:41 +0700

    Remove support

--- src/main/java/me/raviel/lib/RavielLib.java
+++ src/main/java/me/raviel/lib/RavielLib.java
@@ -6,19 +6,16 @@ import java.util.Set;
 import org.bukkit.plugin.java.JavaPlugin;
 import me.raviel.lib.plugin.PluginInfo;
 import me.raviel.lib.plugin.PluginVersion;
-import me.raviel.lib.support.SupportContainer;
 
 public class RavielLib extends JavaPlugin {
 
     private static RavielLib instance;
     private static int libversion;
     private static Set<PluginInfo> rplugins = new HashSet<>();
-    private SupportContainer suppcont = null;
 
     @Override
     public void onEnable(){
         instance = this;
-        suppcont = new SupportContainer();
         parseLibVersion();
         return;
     }
@@ -52,10 +49,6 @@ public class RavielLib extends JavaPlugin {
 
     public static void register(PluginInfo plugininfo){
         rplugins.add(plugininfo);
-    }
-
-    public SupportContainer getSupport(){
-        return this.suppcont;
     }
     
 }
--- src/main/java/me/raviel/lib/support/SupportContainer.java
+++
@@ -1,9 +0,0 @@
-package me.raviel.lib.support;
-
-import me.raviel.lib.RavielLib;
-
-public class SupportContainer {
-
-    public static Vault VAULT_SUPPORT = new Vault(RavielLib.getLib());
-    
-}
