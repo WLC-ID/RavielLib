42a587f6be67288db0224a7b01f86d27e0ea9acc
ravielze <ravielz20@gmail.com>
2021-01-03 21:01:15 +0700

    Fix color hex crash

--- src/main/java/me/raviel/lib/chat/ColorContainer.java
+++ src/main/java/me/raviel/lib/chat/ColorContainer.java
@@ -5,6 +5,8 @@ import me.raviel.lib.utils.ColorUtils;
 
 import java.awt.*;
 
+import org.bukkit.Bukkit;
+
 public class ColorContainer {
 
     private ColorCode colorCode;
@@ -34,10 +36,20 @@ public class ColorContainer {
     public ColorCode getColor() {
         if (colorCode != null) return colorCode;
         if (hexCode == null) return null;
-        java.awt.Color jColor = new Color(
-                Integer.valueOf(hexCode.substring(0, 2), 16),
-                Integer.valueOf(hexCode.substring(2, 4), 16),
-                Integer.valueOf(hexCode.substring(4, 6), 16));
+        java.awt.Color jColor = new Color(255,255,255);
+        try {
+            jColor = new Color(
+                    Integer.valueOf(hexCode.substring(0, 2), 16),
+                    Integer.valueOf(hexCode.substring(2, 4), 16),
+                    Integer.valueOf(hexCode.substring(4, 6), 16));
+        } catch (NumberFormatException ex){
+            try {
+                jColor = new Color(
+                        Integer.valueOf(hexCode.substring(1, 3), 16),
+                        Integer.valueOf(hexCode.substring(3, 5), 16),
+                        Integer.valueOf(hexCode.substring(5, 7), 16));
+            } catch (NumberFormatException ex2){ }
+        }
         return ColorUtils.fromRGB(jColor.getRed(), jColor.getGreen(), jColor.getBlue());
     }
     
